8acc1b8c3f2b3b7f4dcc95e669212bb70f17b525
Denis Calin <deniscalin@Deniss-MacBook-Pro.local>
2024-08-08 19:45:47 -0700

    Add from_pretrained classmethod to GPT()

--- train_gpt2.py
+++ train_gpt2.py
@@ -71,11 +71,11 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embed: int = 384
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embed: int = 768
 
 
 class GPT(nn.Module):
@@ -92,3 +92,58 @@ class GPT(nn.Module):
         ))
 
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    
+    @classmethod
+    def from_pretrained(cls, model_type):
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("Loading weights from pretrained gpt: %s" % model_type)
+
+        # Create the config_args dict
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embed=768), # 124M params
+            'gpt-medium': dict(n_layer=24, n_head=16, n_embed=1024), # 350M params
+            'gpt-large': dict(n_layer=36, n_head=20, n_embed=1280), # 774M params
+            'gpt-xl': dict(n_layer=48, n_head=25, n_embed=1600) # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+
+        # Create our local GPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config=config)
+
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
+
+        # Instantiate a huggingface model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
+        transposed = ['.attn.c_attn.weight', '.attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        
+        assert len(sd_keys_hf) == len(sd_keys), f"Mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+#--------------------------------------------------------------
+model = GPT.from_pretrained('gpt2')
+print("Didn't crash! Nice!!")
+
+
+
