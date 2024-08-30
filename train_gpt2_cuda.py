Fix another shard index integer precision; add shard loading into DataLoader init

train_gpt2_cuda.py
@@ -19,23 +19,23 @@ def load_tokens(filename):
 
 class DataLoader:
 
-    def __init__(self, B, T, process_rank, total_processes):
+    def __init__(self, B, T, process_rank, total_processes, split):
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.total_processes = total_processes
+        assert split in {'train', 'val'}
 
-        # Load tokens from disc at object init
-        # Encode 
-        with open('input.txt', 'r') as f:
-            text = f.read()
-        enc = tiktoken.get_encoding('gpt2')
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f"Loaded {len(tokens)} tokens")
-        # print(f"1 epoch = {len(tokens) // (self.B * self.T * self.total_processes)} batches")
+        # Get the list of shards from the dir, turn into full filenames
+        data_root = 'edu_fineweb10B'
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
 
         # Keeping state
+        self.current_shard = 0
         self.current_position = self.B * self.T * self.process_rank
 
 